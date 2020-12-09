57a53b0b2a8b194841a73e773e05605bb96b3cda
oliveiraFab16 <fabriciocarvalho932@gmail.com>
2020-12-09 11:14:17 -0300

    tabuada

---
+++ Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            int Numero;
+                                   
+            Console.WriteLine("----HOJE IREMOS APRENDER A TABUADA----");
+            Console.WriteLine("Digite um Número: ");
+           
+            Numero = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+                        
+            if (Numero <=1)
+            {           
+            Console.Write("OTIMO VOCÊ ESCOLHEU A TABUADA DO NUMERO 1 ENTÃO VAMOS LA!!!");
+            Console.WriteLine("1 X 0 = 0");
+            Console.WriteLine("1 X 1 = 1");
+            Console.WriteLine("1 X 2 = 2");
+            Console.WriteLine("1 X 3 = 3");
+            Console.WriteLine("1 X 4 = 4");
+            Console.WriteLine("1 X 5 = 5");
+            Console.WriteLine("1 X 6 = 6");
+            Console.WriteLine("1 X 7 = 7");
+            Console.WriteLine("1 X 8 = 8");
+            Console.WriteLine("1 X 9 = 9");
+            Console.WriteLine("1 X 10 = 10");
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+            }
+
+            else if (Numero <=2)
+            {
+            Console.WriteLine("OTIMO VOCÊ ESCOLHEU A TABUADA DO NUMERO 2 ENTÃO VAMOS LA!!!");
+            Console.WriteLine("2 X 0 = 0");
+            Console.WriteLine("2 X 1 = 2");
+            Console.WriteLine("2 X 2 = 4");
+            Console.WriteLine("2 X 3 = 6");
+            Console.WriteLine("2 X 4 = 8");
+            Console.WriteLine("2 X 5 = 10");
+            Console.WriteLine("2 X 6 = 12");
+            Console.WriteLine("2 X 7 = 14");
+            Console.WriteLine("2 X 8 = 16");
+            Console.WriteLine("2 X 9 = 18");
+            Console.WriteLine("2 X 10 = 20 ");
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+            }
+            
+            else if (Numero <=3)
+            {                          
+            Console.WriteLine("OTIMO VOCÊ ESCOLHEU A TABUADA DO NUMERO 3 ENTÃO VAMOS LA!!!");
+            Console.WriteLine("3 X 0 = 0");
+            Console.WriteLine("3 X 1 = 3 ");
+            Console.WriteLine("3 X 2 = 6");
+            Console.WriteLine("3 X 3 = 9");
+            Console.WriteLine("3 X 4 = 12");
+            Console.WriteLine("3 X 5 = 15");
+            Console.WriteLine("3 X 6 = 18");
+            Console.WriteLine("3 X 7 = 21");
+            Console.WriteLine("3 X 8 = 24");
+            Console.WriteLine("3 X 9 = 27");
+            Console.WriteLine("3 X 10 = 30");
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+            }
+
+            else if (Numero <=4)
+            {           
+            Console.WriteLine("OTIMO VOCÊ ESCOLHEU A TABUADA DO NUMERO 4 ENTÃO VAMOS LA!!!");
+            Console.WriteLine("4 X 0 = 0");
+            Console.WriteLine("4 X 1 = 4 ");
+            Console.WriteLine("4 X 2 = 8");
+            Console.WriteLine("4 X 3 = 12");
+            Console.WriteLine("4 X 4 = 16");
+            Console.WriteLine("4 X 5 = 20");
+            Console.WriteLine("4 X 6 = 24");
+            Console.WriteLine("4 X 7 = 28");
+            Console.WriteLine("4 X 8 = 32");
+            Console.WriteLine("4 X 9 = 36");
+            Console.WriteLine("4 X 10 = 40");
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+            }
+
+           else if (Numero <=5)
+
+           {               
+            Console.WriteLine("OTIMO VOCÊ ESCOLHEU A TABUADA DO NUMERO 5 ENTÃO VAMOS LA!!!");
+            Console.WriteLine("5 X 0 = 0");
+            Console.WriteLine("5 X 1 = 5 ");
+            Console.WriteLine("5 X 2 = 10");
+            Console.WriteLine("5 X 3 = 15");
+            Console.WriteLine("5 X 4 = 20");
+            Console.WriteLine("5 X 5 = 25");
+            Console.WriteLine("5 X 6 = 30");
+            Console.WriteLine("5 X 7 = 35");
+            Console.WriteLine("5 X 8 = 40");
+            Console.WriteLine("5 X 9 = 45");
+            Console.WriteLine("5 X 10 = 50");
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+           }
+      
+           else if (Numero <=6)
+
+           {              
+            Console.WriteLine("OTIMO VOCÊ ESCOLHEU A TABUADA DO NUMERO 6 ENTÃO VAMOS LA!!!");
+            Console.WriteLine("6 X 0 = 0");
+            Console.WriteLine("6 X 1 = 6 ");
+            Console.WriteLine("6 X 2 = 12");
+            Console.WriteLine("6 X 3 = 18");
+            Console.WriteLine("6 X 4 = 24");
+            Console.WriteLine("6 X 5 = 30");
+            Console.WriteLine("6 X 6 = 36");
+            Console.WriteLine("6 X 7 = 42");
+            Console.WriteLine("6 X 8 = 48");
+            Console.WriteLine("6 X 9 = 54");
+            Console.WriteLine("6 X 10 =60");
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+           }
+
+          else if (Numero <=7)
+
+          {           
+            Console.WriteLine("OTIMO VOCÊ ESCOLHEU A TABUADA DO NUMERO 7 ENTÃO VAMOS LA!!!");
+            Console.WriteLine("7 X 0 = 0");
+            Console.WriteLine("7 X 1 = 7 ");
+            Console.WriteLine("7 X 2 = 14");
+            Console.WriteLine("7 X 3 = 21");
+            Console.WriteLine("7 X 4 = 28");
+            Console.WriteLine("7 X 5 = 35");
+            Console.WriteLine("7 X 6 = 42");
+            Console.WriteLine("7 X 7 = 49");
+            Console.WriteLine("7 X 8 = 56");
+            Console.WriteLine("7 X 9 = 63");
+            Console.WriteLine("7 X 10 =70");
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+           }
+
+          else if (Numero <=8)
+
+          {
+            Console.WriteLine("OTIMO VOCÊ ESCOLHEU A TABUADA DO NUMERO 8 ENTÃO VAMOS LA!!!");
+            Console.WriteLine("8 X 0 = 0");
+            Console.WriteLine("8 X 1 = 8 ");
+            Console.WriteLine("8 X 2 = 16");
+            Console.WriteLine("8 X 3 = 24");
+            Console.WriteLine("8 X 4 = 32");
+            Console.WriteLine("8 X 5 = 40");
+            Console.WriteLine("8 X 6 = 48");
+            Console.WriteLine("8 X 7 = 56");
+            Console.WriteLine("8 X 8 = 64");
+            Console.WriteLine("8 X 9 = 72");
+            Console.WriteLine("8 X 10 =80");
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+           } 
+
+           else if (Numero <=9)
+
+           {            
+            Console.WriteLine("OTIMO VOCÊ ESCOLHEU A TABUADA DO NUMERO 9 ENTÃO VAMOS LA!!!");
+            Console.WriteLine("9 X 0 = 0");
+            Console.WriteLine("9 X 1 = 9");
+            Console.WriteLine("9 X 2 = 18");
+            Console.WriteLine("9 X 3 = 27");
+            Console.WriteLine("9 X 4 = 36");
+            Console.WriteLine("9 X 5 = 45");
+            Console.WriteLine("9 X 6 = 54");
+            Console.WriteLine("9 X 7 = 63");
+            Console.WriteLine("9 X 8 = 72");
+            Console.WriteLine("9 X 9 = 81");
+            Console.WriteLine("9 X 10 =90");
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+           } 
+
+            else if (Numero <=10)
+
+          {            
+            Console.WriteLine("ÓTIMO VOCÊ ESCOLHEU A TABUADA DO NUMERO 10 ENTÃO VAMOS LÁ!!!");
+            Console.WriteLine("10 X 0 = 0");
+            Console.WriteLine("10 X 1 = 10 ");
+            Console.WriteLine("10 X 2 = 20");
+            Console.WriteLine("10 X 3 = 30");
+            Console.WriteLine("10 X 4 = 40");
+            Console.WriteLine("10 X 5 = 50");
+            Console.WriteLine("10 X 6 = 60");
+            Console.WriteLine("10 X 7 = 70");
+            Console.WriteLine("10 X 8 = 80");
+            Console.WriteLine("10 X 9 = 90");
+            Console.WriteLine("10 X 10 =100");
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+          }
+
+        }
+    }
+}
